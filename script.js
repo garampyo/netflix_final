96cd986a30730472c9172143f12a6380594015db
pgr <2s2rkfkas2s@gmail.com>
2025-02-18 19:36:42 +0900

    secondcommit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // API
-import { API_KEY } from "./env.js";
+// import { API_KEY } from "./env.js";
 
 const tmdbCommand = "https://api.themoviedb.org/3";
 
 const fetchMovies1 = async () => {
-  const url = `${tmdbCommand}/movie/now_playing?api_key=${API_KEY}&language=ko-kr&page=1`;
+  const url = `${tmdbCommand}/movie/now_playing?api_key=d2a72e143d40bab7f709c8672b90046b&language=ko-kr&page=1`;
   const response = await fetch(url);
   const { results } = await response.json();
   return results;
 };
 const fetchMovies2 = async () => {
-  const url = `${tmdbCommand}/movie/upcoming?api_key=${API_KEY}&language=ko-kr&page=1`;
+  const url = `${tmdbCommand}/movie/upcoming?api_key=d2a72e143d40bab7f709c8672b90046b&language=ko-kr&page=1`;
   const response = await fetch(url);
   const { results } = await response.json();
   return results;
